0485b36d7ecb078d1d573a90959b77f9782e28b3
Vladas Zakrevskis <u.zakreuskis@vrweartek.com>
2018-05-08 10:39:28 +0300

    added projects, cmake fix

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,8 @@ function touch { $null >> $args }
 function subl { . 'C:\Program Files\Sublime Text 3\subl.exe' $args }
 function vs { . "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" $args }
 
+$env:Path += 'C:\Program Files\CMake\bin\'
+
 #include browser.ps1
 
 . "$PSScriptRoot/browser.ps1"
@@ -13,6 +15,8 @@ function cdal { cd "C:\Users\$env:username\Documents\WindowsPowerShell" }
 function al { cdal; subl Microsoft.PowerShell_profile.ps1 }
 function alb { cdal; subl browser.ps1 }
 function alp { cdal; subl projects.ps1 }
+
+function cdl { cd "$args"; ls }
 
 function op { ii . }
 function res { Start-Process PowerShell; exit }
@@ -40,3 +44,9 @@ echo hello $env:username
 
 
 hm
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
--- projects.ps1
+++ projects.ps1
@@ -1,1 +1,2 @@
 function node { work; vs "node/build/node.sln" }
+function spp { dev; vs "sand\spp\spp.sln" }
