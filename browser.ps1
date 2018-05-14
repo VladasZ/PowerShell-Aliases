7f70b507df2b8ea1f3e504e0f895fdb6e788b52b
Vladas Zakrevskis <146100@gmail.com>
2018-05-15 00:07:13 +0300

    ...

--- browser.ps1
+++ browser.ps1
@@ -1,5 +1,7 @@
 function vk { ch 'vk.com' }
 function post { ch 'https://webmail.vrweartek.com' }
 function lab { ch 'https://gitlab.vrweartek.com/dashboard/projects' }
-function mygit { ch 'https://github.com/VladasZ' }
 function vrmail { ch 'https://webmail.vrweartek.com' }
+function tele  { ch 'https://web.telegram.org/#/im' }
+function habr  { ch 'https://habr.com/'             }
+function mygit { ch 'https://github.com/VladasZ'    }
