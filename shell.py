8c13553728a934945be00abdf2b7effb8c55cb6b
Vladas Zakrevskis <146100@gmail.com>
2018-07-31 22:57:58 +0300

    ...

--- shell.py
+++ shell.py
@@ -5,24 +5,32 @@ args = '$@'
 
 Function.generate([
 
-    ('r', 'mkdir $@'),
+     ('r', 'mkdir $@')
 
-    ('py', 'python3 $@'),
+    ,('py', 'sudo python3 $@')
 
-    ('_py', 'python3 ' + aldir + '/helper.py $@'),
+    ,('_py', 'python3 ' + aldir + '/helper.py $@')
 
-    ('res', 'py ' + aldir + '/shell.py;. ' + aldir + '/shell.sh'),
+    ,('res', 'py ' + aldir + '/shell.py;. ' + aldir + '/shell.sh')
 
-    ('subll', 'subl .'),
+    ,('subll', 'subl .')
 
-    ('dev', 'cd  ~/dev'),
-    ('sand', 'cd ~/dev/sand'),
+    ,('dev', 'cd  ~/dev')
+    ,('sand', 'cd ~/dev/sand')
 
-    ('cdal', 'cd ' + aldir),
-    ('al', 'cdal\n subll'),
+    ,('cdal', 'cd ' + aldir)
+    ,('al', 'cdal\n subll')
 
-    ('op', 'open .'),
+    ,('op', 'xdg-open .')
 
-    ('getbuild', '_py cleanclone vladasz/build'),
-    ('build', 'py build.py')
+    ,('getbuild', '_py cleanclone vladasz/build')
+    ,('build', 'py build.py')
+
+##projects
+
+    ,('cdengine', 'cd ~/dev/projects/testengine')
+    ,('bengine', 'cdengine;cd build;sudo make;cd ..')
+    ,('rengine', 'cdengine;cd build/source/Engine/bin/; ./Engine; cdengine')
+    ,('engine', 'bengine;rengine')
+
 ])
